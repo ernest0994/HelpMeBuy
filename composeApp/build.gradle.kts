e151f56b249014691fb7a2d45caaec21859cadff
Ernesto Acosta <eacosta0994@gmail.com>
2025-09-06 14:36:07 -0400

    cleaning more warnings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,10 +39,6 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.shared)
         }
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/com/helpmebuyapp/helpmebuy/App.kt
+++ composeApp/src/commonMain/kotlin/com/helpmebuyapp/helpmebuy/App.kt
@@ -1,26 +1,8 @@
 package com.helpmebuyapp.helpmebuy
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import helpmebuy.composeapp.generated.resources.Res
-import helpmebuy.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.runtime.Composable
 
 @Composable
-@Preview
 fun App() {
     // Make Lists the default screen
     com.helpmebuyapp.helpmebuy.ui.ListsScreen(
